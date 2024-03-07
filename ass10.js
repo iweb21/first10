723a81db2fba6d3f4026f7aa5305927aa601c43e
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-03-07 19:09:14 +0500

    Add files via upload

---
+++ ass10.js
@@ -0,0 +1,8 @@
+"use strict";
+// Adding Comment:choose two of the program you written ,and add atleast one comment to each.is you don't have anything specific to write current date at the top of each program file.Then write one sentence describing what the program does.
+let a = 10;
+let b = 20;
+console.log(`${a} + ${b} = ${a + b}`);
+let a1 = 10;
+let b1 = 20;
+console.log(`${a1} - ${b1} = ${a1 - b1}`);
---
+++ ass10.ts
@@ -0,0 +1,9 @@
+// Adding Comment:choose two of the program you written ,and add atleast one comment to each.is you don't have anything specific to write current date at the top of each program file.Then write one sentence describing what the program does.
+
+let a:number = 10
+let b:number = 20
+console.log(`${a} + ${b} = ${a+b}`)
+
+let a1:number = 10
+let b1:number = 20
+console.log(`${a1} - ${b1} = ${a1-b1}`)
